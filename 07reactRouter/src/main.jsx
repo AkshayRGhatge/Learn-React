4b149399d6e77c93a082b1ee2ffed5af1eec0e7e
Akshay <akshayghatge6011@gmail.com>
2025-10-21 21:56:43 -0400

    Learned react router defining using createRoutesFromElements, also added few components

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider,Route } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import About from './component/About/About.jsx'
 import Home from './component/Home/Home.jsx'
+import Contact from './component/Contact/Contact.jsx'
+import User from './component/User/User.jsx'
+import Github from './component/Github/Github.jsx'
 
-
+/*
 const router = createBrowserRouter ([
   {
     path:'/',
@@ -20,10 +23,28 @@ const router = createBrowserRouter ([
       {
         path: "about",
         element:<About />
+      },
+      {
+        path:"contact",
+        element:<Contact />
       }
     ]
   }
 ])
+*/
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route path="github" element={<Github />} />
+    </Route>
+  )
+);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
