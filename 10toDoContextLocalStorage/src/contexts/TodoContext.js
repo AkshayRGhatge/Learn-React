a7c7a7f5963168c036bf362093f54abe05e28bcc
Akshay <akshayghatge6011@gmail.com>
2025-10-30 21:40:48 -0400

    Added index.js and updated context and app.jsx

--- 10toDoContextLocalStorage/src/contexts/TodoContext.js
+++ 10toDoContextLocalStorage/src/contexts/TodoContext.js
@@ -2,7 +2,19 @@
 import { createContext, useContext } from "react";
 
 //Create Context
-export const TodoContext= createContext({});
+export const TodoContext= createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Learn React",
+            completed:false
+        }
+    ],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{},
+});
 
 //Make Context available to child components by creating provider
 export const TodoProvider=TodoContext.Provider;
